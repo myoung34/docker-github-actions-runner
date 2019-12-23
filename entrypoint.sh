5cb4c4ad6f40bd5da871ea38906b8a5aeaff27d0
Marcus Young <myoung34@my.apsu.edu>
2019-12-23 09:47:23 -0600

    Fix '--agent' -> '--name', add ability for arm64 builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ export AGENT_ALLOW_RUNASROOT=1
 export PATH=$PATH:/actions-runner
 _RUNNER_NAME=${RUNNER_NAME:-default}
 _RUNNER_WORKDIR=${RUNNER_WORKDIR:-/_work}
-config.sh --url "${REPO_URL}" --token "${RUNNER_TOKEN}" --agent "${_RUNNER_NAME}" --work "${_RUNNER_WORKDIR}"
+config.sh --url "${REPO_URL}" --token "${RUNNER_TOKEN}" --name "${_RUNNER_NAME}" --work "${_RUNNER_WORKDIR}"
 exec run.sh
