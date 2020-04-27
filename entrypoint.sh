6ea8dfe97b0cd153e1833f99e95d1f67aea7f8dd
Andrey Astashov <mvc.aaa@gmail.com>
2020-04-27 19:33:44 +0300

    Remove feature for auto-scaling

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,4 +47,11 @@ echo "Configuring"
     --unattended \
     --replace
 
+remove() {
+    ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
+}
+
+trap 'remove; exit 130' INT
+trap 'remove; exit 143' TERM
+
 ./run.sh
