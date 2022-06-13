22c084e61b043d5c615c57244e8677810f8a97d9
koichi.sakamoto <sakamoto0321@gmail.com>
2022-06-14 04:10:37 +0900

    refacting for sider check

--- check_jobs.sh
+++ check_jobs.sh
@@ -6,14 +6,17 @@ _JOBS_ACCEPTANCE_TIMEOUT=$1
 _CHECK_DIR=/actions-runner/_diag/pages
 
 sleep 1
-while [[ -n `pgrep Runner.Listener` ]]; do
+while [[ -n $(pgrep Runner.Listener) ]]; do
   sleep 10
-  _DURATION=`expr $(date +%s) - $(date +%s -r /tmp)`
+  _NOW=$(date +%s)
+  _LASTUPDATE=$(date +%s -r /tmp)
+  _DURATION=$(expr ${_NOW} - ${_LASTUPDATE})
   [[ -e ${_CHECK_DIR} ]] && [[ -n "$(ls -A ${_CHECK_DIR})" ]] && continue
   [[ ${_DURATION} -lt ${_JOBS_ACCEPTANCE_TIMEOUT} ]] && continue
 
   echo $(date '+%Y-%m-%d %H:%M:%SZ:') Stop listening due to timeout.
   pkill --signal=SIGINT -f ./bin/Runner.Listener
-  break
+  exit 0
 done
+echo $(date '+%Y-%m-%d %H:%M:%SZ:') Skip check jobs.
 exit 0
